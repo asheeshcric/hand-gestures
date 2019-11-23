83f341596d6830cf6cb729ca99adcda936f9e461
Ashish <ashiz2013@gmail.com>
2019-11-23 00:57:45 -0600

    Refactor code for dataset collection

--- collect_hand_data.py
+++ collect_hand_data.py
@@ -1,33 +1,23 @@
 import cv2
 import imutils
-import numpy as np
 import os
 import time
 
 
-# --------------------------------------------------
-# To find the running average over the background
-# --------------------------------------------------
+def average_background(image, avg_weight, background):
+    # Initialize background if for the first time
+    if background is None:
+        background = image.copy().astype("float")
+        return background
+
+    # Compute weighted average for the background for the first few seconds
+    cv2.accumulateWeighted(image, background, avg_weight)
+    return background
 
 
-def run_avg(image, aWeight):
-    global bg
-    # initialize the background
-    if bg is None:
-        bg = image.copy().astype("float")
-        return
-
-    # compute weighted average, accumulate it and update the background
-    cv2.accumulateWeighted(image, bg, aWeight)
-
-
-# ---------------------------------------------
-# To segment the region of hand in the image
-# ---------------------------------------------
-def segment(image, threshold=25):
-    global bg
-    # find the absolute difference between background and current frame
-    diff = cv2.absdiff(bg.astype("uint8"), image)
+def segment(image, background, threshold=25):
+    # Finding the absolute difference between image and the background
+    diff = cv2.absdiff(background.astype("uint8"), image)
 
     # threshold the diff image so that we get the foreground
     thresholded = cv2.threshold(diff, threshold, 255, cv2.THRESH_BINARY)[1]
@@ -38,104 +28,102 @@ def segment(image, threshold=25):
 
     # return None, if no contours detected
     if len(cnts) == 0:
-        return
+        return None
     else:
         # based on contour area, get the maximum contour which is the hand
         segmented = max(cnts, key=cv2.contourArea)
         return thresholded, segmented
 
 
-# -----------------
-# MAIN FUNCTION
-# -----------------
-if __name__ == "__main__":
-    # initialize weight for running average
-    aWeight = 0.5
+def get_gray_image(cap, dims):
+    # Retrieve the current frame
+    _, frame = cap.read()
 
-    # get the reference to the webcam
-    camera = cv2.VideoCapture(0)
+    # Resize the frame
+    frame = imutils.resize(frame, width=700)
+    # Flip the camera frame (to avoid mirror image)
+    frame = cv2.flip(frame, 1)
+    # Create a different copy of the frame
+    clone = frame.copy()
 
-    # region of interest (ROI) coordinates
-    top, bottom, left, right = 10, 325, 690, 350
+    # Crop the Region of Interest (RoI) from the entire frame
+    roi = frame[dims['top']:dims['bottom'], dims['right']:dims['left']]
 
-    # initialize num of frames
+    # Convert RoI to grayscale and apply Gaussian blurring
+    gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (7, 7), 0)
+
+    return gray, clone
+
+
+def collect_images():
+    # Initialize all the constant values at the beginning
+    background = None
+    avg_weight = 0.5
+    dims = {'top': 10, 'bottom': 325, 'left': 690, 'right': 350}
     num_frames = 0
 
-    # keep looping, until interrupted
-    store_path = './datasets/new_hand_contours/ok'
+    # Details for storage of image data
+    image_class = 'left'
+    store_path = f'./datasets/new_hand_contours/{image_class}'
     if not os.path.exists(store_path):
         os.makedirs(store_path)
     img_count = len(os.listdir(store_path))
     this_count = 1
     store = False
     start_time = time.time()
+
+    # Capture frames from the webcam
+    cap = cv2.VideoCapture(0)
+
     while True:
-        # get the current frame
-        (grabbed, frame) = camera.read()
+        # Handle interrupts from the user for different modes
+        keypress = cv2.waitKey(1) & 0xFF
+        # If 't' is pressed, start classifying the frames
+        if keypress == ord('t'):
+            store = True
 
-        # resize the frame
-        frame = imutils.resize(frame, width=700)
+        # If 'z' is pressed, end the program
+        if keypress == ord("z") or this_count % 600 == 0:
+            break
 
-        # flip the frame so that it is not the mirror view
-        frame = cv2.flip(frame, 1)
+        # Get processed gray image along with the original frame
+        gray, original_frame = get_gray_image(cap, dims)
 
-        # clone the frame
-        clone = frame.copy()
-
-        # get the height and width of the frame
-        (height, width) = frame.shape[:2]
-
-        # get the ROI
-        roi = frame[top:bottom, right:left]
-
-        # convert the roi to grayscale and blur it
-        gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-        gray = cv2.GaussianBlur(gray, (7, 7), 0)
-
-        # to get the background, keep looking till a threshold is reached
-        # so that our running average model gets calibrated
+        # To calibrate the running average model and get the background, keep looking till a threshold is reached
+        # 30 allows it to calibrate for 1 sec (30 frames/sec rate)
         if num_frames < 30:
-            run_avg(gray, aWeight)
+            background = average_background(gray, avg_weight, background)
         else:
-            # segment the hand region
-            hand = segment(gray)
+            # Segment the hand portion
+            hand = segment(gray, background)
 
-            # check whether hand region is segmented
+            # Proceed only if the hand region is segmented
             if hand is not None:
-                # if yes, unpack the thresholded image and
-                # segmented region
-                (thresholded, segmented) = hand
+                thresholded, segmented = hand
+                # Display the segmented hand using the thresholded values for the hand
+                cv2.drawContours(original_frame, [segmented + (dims['right'], dims['top'])], -1, (0, 0, 255))
+                cv2.imshow("Hand Contour (B/W)", thresholded)
 
-                # draw the segmented region and display the frame
-                cv2.drawContours(
-                    clone, [segmented + (right, top)], -1, (0, 0, 255))
-                cv2.imshow("Thesholded", thresholded)
                 if store and time.time() - start_time > 0.05:
+                    # Store images for the particular class at certain intervals
                     img_count += 1
                     this_count += 1
                     cv2.imwrite(f'{store_path}/image_{img_count}.png', thresholded)
 
-        # draw the segmented hand
-        cv2.rectangle(clone, (left, top), (right, bottom), (0, 255, 0), 2)
+        # Draw a rectangle to define the Region of Interest
+        cv2.rectangle(original_frame, (dims['left'], dims['top']), (dims['right'], dims['bottom']), (0, 255, 0), 2)
 
-        # increment the number of frames
+        # Counter for the number of frames displayed so far
         num_frames += 1
 
-        # display the frame with segmented hand
-        cv2.imshow("Video Feed", clone)
+        # Display the original frame
+        cv2.imshow("Original Frames", original_frame)
 
-        # observe the keypress by the user
-        keypress = cv2.waitKey(1) & 0xFF
-
-        # if the user pressed "q", then stop looping
-        if keypress == ord("q") or this_count % 600 == 0:
-            break
-
-        if keypress == ord('t'):
-            store = True
-            start_time = time.time()
-            print('Writing images to the dataset...')
-
-    # free up memory
-    camera.release()
+    # Release the webcam
+    cap.release()
     cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    collect_images()
