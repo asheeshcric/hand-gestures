5e4bef867237b8fbced098c782d98bda8632c889
Ashish <ashiz2013@gmail.com>
2019-11-22 22:37:21 -0600

    Make code robust and error free when predicting a class

--- predict_gesture.py
+++ predict_gesture.py
@@ -1,8 +1,5 @@
 import cv2
 import imutils
-import numpy as np
-import os
-import time
 
 from PIL import Image
 
@@ -11,8 +8,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torchvision import transforms
-
-bg = None
 
 
 # CNN Network for the trained model
@@ -46,29 +41,20 @@ def predict_image():
     return classes[predicted[0]]
 
 
-# --------------------------------------------------
-# To find the running average over the background
-# --------------------------------------------------
+def average_background(image, avg_weight, background):
+    # Initialize background if for the first time
+    if background is None:
+        background = image.copy().astype("float")
+        return background
+
+    # Compute weighted average for the background for the first few seconds
+    cv2.accumulateWeighted(image, background, avg_weight)
+    return background
 
 
-def run_avg(image, aWeight):
-    global bg
-    # initialize the background
-    if bg is None:
-        bg = image.copy().astype("float")
-        return
-
-    # compute weighted average, accumulate it and update the background
-    cv2.accumulateWeighted(image, bg, aWeight)
-
-
-# ---------------------------------------------
-# To segment the region of hand in the image
-# ---------------------------------------------
-def segment(image, threshold=25):
-    global bg
+def segment(image, background, threshold=25):
     # find the absolute difference between background and current frame
-    diff = cv2.absdiff(bg.astype("uint8"), image)
+    diff = cv2.absdiff(background.astype("uint8"), image)
 
     # threshold the diff image so that we get the foreground
     thresholded = cv2.threshold(diff, threshold, 255, cv2.THRESH_BINARY)[1]
@@ -79,16 +65,95 @@ def segment(image, threshold=25):
 
     # return None, if no contours detected
     if len(cnts) == 0:
-        return
+        return None
     else:
         # based on contour area, get the maximum contour which is the hand
         segmented = max(cnts, key=cv2.contourArea)
         return thresholded, segmented
 
 
-# -----------------
-# MAIN FUNCTION
-# -----------------
+def get_gray_image(cap, dims):
+    # get the current frame
+    grabbed, frame = cap.read()
+
+    # Resize the frame
+    frame = imutils.resize(frame, width=700)
+    # Flip the camera frame (to avoid mirror image)
+    frame = cv2.flip(frame, 1)
+    # Create a different copy of the frame
+    clone = frame.copy()
+
+    # Crop the Region of Interest (RoI) from the entire frame
+    roi = frame[dims['top']:dims['bottom'], dims['right']:dims['left']]
+
+    # Convert RoI to grayscale and apply Gaussian blurring
+    gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (7, 7), 0)
+
+    return gray, clone
+
+
+def main():
+    # Initialize all the constant values at the beginning
+    background = None
+    avg_weight = 0.5
+    dims = {'top': 10, 'bottom': 325, 'left': 690, 'right': 350}
+    num_frames = 0
+    classify = False
+    # Font for text displayed on the image window
+    font = cv2.FONT_HERSHEY_SIMPLEX
+
+    # get the reference to the webcam
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        # Handle interrupts from the user for different modes
+        keypress = cv2.waitKey(1) & 0xFF
+        # If 't' is pressed, start classifying the frames
+        if keypress == ord('t'):
+            classify = True
+
+        # If 'z' is pressed, end the program
+        if keypress == ord("z"):
+            break
+
+        # Get processed gray image along with the original frame
+        gray, original_frame = get_gray_image(cap, dims)
+
+        # To calibrate the running average model and get the background, keep looking till a threshold is reached
+        # 30 allows it to calibrate for 1 sec (30 frames/sec rate)
+        if num_frames < 30:
+            background = average_background(gray, avg_weight, background)
+        else:
+            # Segment the hand portion
+            hand = segment(gray, background)
+
+            # Proceed only if the hand region is segmented
+            if hand is not None:
+                thresholded, segmented = hand
+                # Display the segmented hand using the thresholded values for the hand
+                cv2.drawContours(original_frame, [segmented + (dims['right'], dims['top'])], -1, (0, 0, 255))
+                cv2.imshow("Hand Contour (B/W)", thresholded)
+
+                if classify:
+                    cv2.imwrite('output.jpg', thresholded)
+                    prediction = predict_image()
+                    cv2.putText(original_frame, prediction, (0, 50), font, 2, (0, 0, 255), 3, cv2.LINE_AA)
+
+        # Draw a rectangle to define the Region of Interest
+        cv2.rectangle(original_frame, (dims['left'], dims['top']), (dims['right'], dims['bottom']), (0, 255, 0), 2)
+
+        # Counter for the number of frames displayed so far
+        num_frames += 1
+
+        # Display the original frame
+        cv2.imshow("Original Frames", original_frame)
+
+    # Release the webcam
+    cap.release()
+    cv2.destroyAllWindows()
+
+
 if __name__ == "__main__":
     # For deep learning model
     classes = ['fist', 'left', 'ok', 'right', 'straight']
@@ -100,87 +165,4 @@ if __name__ == "__main__":
          transforms.ToTensor(),
          transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
     )
-
-    # initialize weight for running average
-    aWeight = 0.5
-
-    # get the reference to the webcam
-    camera = cv2.VideoCapture(0)
-
-    # region of interest (ROI) coordinates
-    top, bottom, left, right = 10, 325, 690, 350
-
-    # initialize num of frames
-    num_frames = 0
-
-    # keep looping, until interrupted
-    classify = False
-    while True:
-        # get the current frame
-        (grabbed, frame) = camera.read()
-
-        # resize the frame
-        frame = imutils.resize(frame, width=700)
-
-        # flip the frame so that it is not the mirror view
-        frame = cv2.flip(frame, 1)
-
-        # clone the frame
-        clone = frame.copy()
-
-        # get the height and width of the frame
-        (height, width) = frame.shape[:2]
-
-        # get the ROI
-        roi = frame[top:bottom, right:left]
-
-        # convert the roi to grayscale and blur it
-        gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-        gray = cv2.GaussianBlur(gray, (7, 7), 0)
-
-        # to get the background, keep looking till a threshold is reached
-        # so that our running average model gets calibrated
-        if num_frames < 30:
-            run_avg(gray, aWeight)
-        else:
-            # segment the hand region
-            hand = segment(gray)
-
-            # check whether hand region is segmented
-            if hand is not None:
-                # if yes, unpack the thresholded image and
-                # segmented region
-                (thresholded, segmented) = hand
-
-                # draw the segmented region and display the frame
-                cv2.drawContours(
-                    clone, [segmented + (right, top)], -1, (0, 0, 255))
-                cv2.imshow("Thesholded", thresholded)
-
-                if classify:
-                    cv2.imwrite('output.jpg', thresholded)
-                    prediction = predict_image()
-                    print(prediction)
-
-        # draw the segmented hand
-        cv2.rectangle(clone, (left, top), (right, bottom), (0, 255, 0), 2)
-
-        # increment the number of frames
-        num_frames += 1
-
-        # display the frame with segmented hand
-        cv2.imshow("Video Feed", clone)
-
-        # observe the keypress by the user
-        keypress = cv2.waitKey(1) & 0xFF
-
-        # if the user pressed "q", then stop looping
-        if keypress == ord("q"):
-            break
-
-        if keypress == ord('t'):
-            classify = True
-
-    # free up memory
-    camera.release()
-    cv2.destroyAllWindows()
+    main()
